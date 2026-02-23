08d55eaa482abdd4f08a335e71299990cf26439c
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2026-02-23 23:59:46 +0530

    forgot-password and reset-password implemented

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -90,6 +90,68 @@ router.post("/login", async (req,res)=>{
         }
     })
 
+//Forgot-password route 
+
+router.post("/forgot-password", async (req, res)=>{
+    try{
+        const {email} = req.body;
+        //check if user exist
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(404).json({message : "User not found"})
+        }
+            // Generate reset token 
+            const resetToken = jwt.sign(
+                {id: user._id},
+                process.env.JWT_SECRET,
+                {expiresIn : "15m"}
+            );
+        // create reset link
+        const resetLink = `http://localhost:3000/reset-password/${resetToken}`
+        await sendEmail(
+            user.email,
+            "Password reset",
+            `Click the link to reset your password : ${resetLink}`
+        );
+        res.status(200).json({message : "Password reset link sent to email"});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message : "Server error"});
+    }
+});
+
+//reset-password route 
+
+router.post("/reset-password", async (req, res)=>{
+    try{
+        //verify token 
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        // find user
+        const user = await User.findById(decoded.id);
+        // check if user exist 
+        if(!user){
+            res.status(404).json({message : "User not found"});
+        }
+
+        // hash new password 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        // update password
+        user.password = hashedPassword; 
+        //save User
+        await user.save();
+        res.status(200).json({message : "Password reset successfully"});
+    }
+    catch(error){
+        console.log(error);
+        if(error.name === "TokenExpiredError"){
+            res.status(400).json({message : "Token expired"});
+        }
+        res.status(500).json({message : "Server error"});
+    }
+})
+
 // Verification Route
 
 router.get("/verify/:token", async (req,res)=>{
