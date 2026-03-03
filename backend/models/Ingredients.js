02ddf1031f2d37119d40935ffca3ba5fbb52b166
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2026-03-04 00:50:14 +0530

    Ingredients and Pizza models are added

---
+++ backend/models/Ingredients.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const ingredientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    type: {
+      type: String,
+      enum: ["base", "sauce", "cheese", "veggie", "meat"],
+      required: true
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Ingredient = mongoose.model("Ingredient", ingredientSchema);
+
+export default Ingredient;
---
+++ backend/models/Pizza.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const pizzaSchema = new mongoose.Schema(
+  {
+    base: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ingredient",
+      required: true,
+    },
+    sauce: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ingredient",
+      required: true,
+    },
+    cheese: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ingredient",
+      required: true,
+    },
+    veggies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ingredient",
+      },
+    ],
+    meats: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ingredient",
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Pizza = mongoose.model("Pizza", pizzaSchema);
+
+export default Pizza;
