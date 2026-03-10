4daf2c8d6e070df50d25d1c3edde765b05da62c5
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2026-03-10 23:19:14 +0530

    Update orderRoute

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,7 +3,7 @@ import Order from "../models/Order.js"
 import Pizza from "../models/Pizza.js"
 import Ingredient from "../models/Ingredients.js"
 import authMiddleware from "../middleware/authMiddleware.js"
-
+import adminMiddleware from "../middleware/adminMiddleware.js"
 const router = express.Router()
 
 // Place Order
@@ -24,26 +24,31 @@ try{
     // Base price
     const base = await Ingredient.findById(pizza.base)
     totalPrice += base.price
+    await Ingredient.findByIdAndUpdate(pizza.base,{$inc :{stock:-1}});
 
     // Sauce price
     const sauce = await Ingredient.findById(pizza.sauce)
     totalPrice += sauce.price
+    await Ingredient.findByIdAndUpdate(pizza.sauce,{$inc :{stock:-1}});
 
     // Cheese price
     const cheese = await Ingredient.findById(pizza.cheese)
     totalPrice += cheese.price
+    await Ingredient.findByIdAndUpdate(pizza.cheese,{$inc :{sock:-1}});
 
     // Veggies price
     const veggies = await Ingredient.find({_id: {$in: pizza.veggies}})
-    veggies.forEach(v => {
+    for(const v of veggies){
         totalPrice += v.price
-    })
+        await Ingredient.findByIdAndUpdate(v._id,{$inc:{stock:-1}});
+    }
 
     // Meat price
     const meat = await Ingredient.find({_id: {$in: pizza.meat}})
-    meat.forEach(m => {
+    for(const m of meat){
         totalPrice += m.price
-    })
+        await Ingredient.findByIdAndUpdate(m._id,{$inc:{stock:-1}});
+    }    
 
     // Create order
     const order = new Order({
@@ -59,7 +64,7 @@ try{
         totalPrice,
         order
     })
-
+ 
 }
 catch(error){
     res.status(500).json({message:error.message})
@@ -67,4 +72,51 @@ catch(error){
 
 })
 
+//status update route 
+
+router.put("/update-status/:orderId", authMiddleware,adminMiddleware, async (req, res)=>{
+    try{
+        const {orderStatus}= req.body;
+        
+        const order = await Order.findByIdAndUpdate(
+            req.params.orderId,
+            {orderStatus},
+            {new:true}
+        )
+        if(!order){
+            return res.status(404).json({message:"Order not found"});
+        }
+        res.json({message :"Order status updated successfully", order});
+    }
+    catch(error){
+        res.status(500).json({message: error.message});
+    }
+})
+
+// Get My Orders
+
+router.get("/my-orders", authMiddleware, async (req,res)=>{
+
+try{
+
+   const orders = await Order.find({ user:req.user.id })
+   .populate({
+      path:"pizza",
+      populate:[
+        {path:"base"},
+        {path:"sauce"},
+        {path:"cheese"},
+        {path:"veggies"},
+        {path:"meat"}
+      ]
+   })
+
+   res.json(orders)
+
+}
+catch(error){
+   res.status(500).json({message:error.message})
+}
+
+})
 export default router
