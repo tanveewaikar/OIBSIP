ba9cf60ed3c1f736188b3db4cd2d0b632cb45c15
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2026-03-11 17:08:59 +0530

    Update ProtectedRoute and Register page

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,15 +1,9 @@
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children, role }) {
-  const auth = JSON.parse(localStorage.getItem("auth"));
-
-  if (!auth) {
-    return <Navigate to="/login" />;
+export default function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token");
+  if(!token){
+   return <Navigate to = "/login"/>;
   }
-
-  if (role && auth.role !== role) {
-    return <Navigate to="/login" />;
-  }
-
   return children;
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,53 +1,65 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 export default function Register() {
 const [name,setName] = useState("")
 const [email,setEmail]= useState("")
 const [password,setPassword] = useState("")
 const [message,setMessage] = useState("")
+const [error , setError] = useState("")
 
 const navigate = useNavigate()
 
-const handleRegister = (e)=>{
+const handleRegister = async (e)=>{
   e.preventDefault();
-  
-  const newUser = {
-    name,
-    email,
-    password
-  };
-  localStorage.setItem("user",JSON.stringify(newUser));
-  setMessage("Registartion successfull Redirecting to login..")
-  
-  setTimeout(()=>{
-    navigate("/");
-  },1500)
+  try{
+    const res = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      {name,email,password}
+    )
+    setMessage(res.data.message);
+    setTimeout (()=>{
+      navigate('/login');
+    },2000)
+  }
+  catch(error){
+    setError(error.response?.data?.message || "Registration failed"
+    )
+  }
 }
 
   return (
-   <div className="container-fluid vh-100  d-flex align-items-center justify-content-center ">
-     <div className="col-md-4">
-       <h1 className="text-center mb-4">Register</h1>
-       {message && (
-        <p className="text-success text-center">{message}</p>
-       )}
-      <form onSubmit={handleRegister}>
-        <div className="mb-3">
-          <label>Name : </label>
-          <input type="text" className="form-control" value={name} onChange={(e)=>setName(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-          <label>Email : </label>
-          <input type="email" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)} />
-        </div>
-        <div className="mb-3">
-          <label>Password : </label>
-          <input type="password" className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)} />
-        </div>
-        <button className="btn btn-success w-100">Register</button>
-      </form>
-     </div>
-   </div>
-  )
+<div className="container vh-100 d-flex justify-content-center align-items-center">
+<div className="col-md-4">
+
+<h2 className="text-center mb-4">Register</h2>
+
+{message && <p className="text-success text-center">{message}</p>}
+{error && <p className="text-danger text-center">{error}</p>}
+
+<form onSubmit={handleRegister}>
+
+<div className="mb-3">
+<label>Name</label>
+<input type="text" className="form-control" value={name} onChange={(e)=>setName(e.target.value)} required/>
+</div>
+
+<div className="mb-3">
+<label>Email</label>
+<input type="email" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)} required />
+</div>
+
+<div className="mb-3">
+<label>Password</label>
+<input type="password" className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+</div>
+
+<button className="btn btn-success w-100">Register</button>
+
+</form>
+
+</div>
+</div>
+)
 }
